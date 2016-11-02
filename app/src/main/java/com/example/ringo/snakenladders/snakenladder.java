da3951a39cfa44a6f61e3cecc19073a0e8bb1dd0
ringo chui <ringo chui>
2016-11-02 17:14:37 -0400

    added layout

---
+++ app/src/main/java/com/example/ringo/snakenladders/snakenladder.java
@@ -0,0 +1,32 @@
+package com.example.ringo.snakenladders;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.ArrayList;
+
+public class snakenladder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snakenladder);
+
+    }
+
+
+
+    public void dice(View v) {
+        Edit
+
+    }
+
+    private String
+}
