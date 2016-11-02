dfc3c59b64dd9ed23102bf09e1693028fd98d9af
ringo chui <ringo chui>
2016-11-02 18:04:05 -0400

    added some conditional statements

--- app/src/main/java/com/example/ringo/snakenladders/snakenladder.java
+++ app/src/main/java/com/example/ringo/snakenladders/snakenladder.java
@@ -4,6 +4,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
@@ -19,14 +21,40 @@ public class snakenladder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snakenladder);
 
+        ArrayList<Integer> start = new ArrayList<Integer>();
+
+
+
+
     }
 
-
+    private int progress = 1;
 
     public void dice(View v) {
-        Edit
+        EditText number = (EditText)findViewById(R.id.number_input);
+        String num = number.getText().toString();
+        int intNum = Integer.parseInt(num);
+
+        TextView results = (TextView)findViewById(R.id.results);
+        results.setText(getPosition(intNum));
 
     }
 
-    private String
+    private String getPosition(int steps){
+
+        if (steps == 0) {
+            System.out.print("You Quit!");
+        }
+
+        else if (steps >= 2 && steps <= 12) {
+            progress += steps;
+            if (progress == 54 || progress == 90 || progress == 99 || progress == 9 || progress == 40 || progress == 67) {
+                for (int j = 0; j < 6; j++) {
+                    if (progress == )
+                    }
+                }
+            }
+        }
+
+    }
 }
